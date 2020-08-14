505510d3fca4aca449476d603c9ab3025f869ffe
biocdgv <biocdgv@gmail.com>
2020-08-14 18:46:19 -0500

    Bowtie finito

--- scripts/bowtie2.sh
+++ scripts/bowtie2.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-echo "Performing bowtie2 alignment. You should have a conda  environment in order to run this script:"
+# Version 1.0
+# 2020-08-14
+
+echo "Performing bowtie2 alignment. You should have a conda environment \
+in order to run this script:"
 
 ##Variables##:
 
-threads=$1
-host=$2
-#name_env=$3
+threads=$1	# Number of threads to use when running Bowtie2
+host=$2		# Genome of the host plant
 env_path="$CONDA_PREFIX/bin"
 original_dir=$(pwd)
 
@@ -30,8 +33,11 @@ fi
 #################Downloading Human and PhiX reference genomes#################:
 : '
 for i in {1..10};do
-	echo "PhiX and human genomes:" && esearch -db nucleotide -query "NC_001422.1" | efetch -format fasta > NC_001422.1.fasta
-	esearch -db nucleotide -query "GCA_000001405.28"  | efetch -format fasta > GCA_000001405.28.fasta
+	echo "PhiX and human genomes:"
+	esearch -db nucleotide -query "NC_001422.1" | \
+	efetch -format fasta > NC_001422.1.fasta
+	esearch -db nucleotide -query "GCA_000001405.28"  | \
+	efetch -format fasta > GCA_000001405.28.fasta
 	echo "Confirming downloaded genomes: "
 	if  [ -e NC_001422.1.fasta ];then
 			echo "PhiX genome was downloaded"
@@ -46,31 +52,24 @@ cd $original_dir
 ###Concatenating genomes to be aligned#####:
 
 #cat $host NC_001422.1.fasta GCA_000001405.28.fasta > Mixed.fasta
-cat $host NC_001422.fasta GCF_000001405.39_GRCh38.p13_genomic > Mixed.fasta # Se eliminó genoma humano porque no estaba todavía en el cluster
+cat $host NC_001422.fasta GCF_000001405.39_GRCh38.p13_genomic.fna > Mixed.fasta
 
 #########Building genome index and bowtie alignment###################:
 echo "Building genome index:"
 bowtie2-build Mixed.fasta Mix --threads $threads && echo 'Indexing genomes to filter out...'
 
 
-############################PE alignment##############################:
-mkdir aligned
+############################Pair end (PE) alignment##############################:
+mkdir unaligned
 	for i in $(ls *f-paired.fq.gz -1);do
-		#cd aligned
-		#touch $(echo $i | sed 's/f-paired.fq.gz/PE_aligned.sam/')
-		#touch $(echo $i | sed 's/f-paired.fq.gz/SE_aligned.sam/')
-		#cd ..
 		echo "PE alignment: "
 		bowtie2 -x Mix -1 $i -2 $(echo $i | sed 's/f-paired/r-paired/') \
-		--un-conc-gz aligned/$(echo $i | sed 's/f-paired.fq.gz/PE_aligned.fq.gz/') \
-		-q -p $threads --met-file $(echo $i | sed 's/f-paired.fq.gz/PE_summary.txt/');done
+		--un-conc-gz unaligned/$(echo $i | sed 's/f-paired.fq.gz/PE_unaligned.fq.gz/') \ #Get unaligned reads
+		-q -p $threads --met-file unaligned/$(echo $i | sed 's/f-paired.fq.gz/PE_summary.txt/');done
 
-############################SE alignment###############################:
+############################Single end (SE) alignment###############################:
 	for i in $(ls *unpaired.fq.gz -1);do
-		#cd aligned
-		#touch $(echo $i | sed 's/unpaired.fastq.gz/unpaired_aligned.sam/')
-		#cd ..
 		echo "SE alignemnt: "
 		bowtie2 -x Mix -U $i \
-		--un-gz aligned/$(echo $i | sed 's/unpaired.fastq.gz/unpaired.fq.gz/') \
-		-q -p $threads --met-file $(echo $i | sed 's/unpaired.fastq.gz/unpaired_summary.txt/');done
+		--un-gz unaligned/$(echo $i | sed 's/unpaired.fastq.gz/unpaired.fq.gz/') \ # Get unaligned reads
+		-q -p $threads --met-file unaligned/$(echo $i | sed 's/unpaired.fastq.gz/SE_summary.txt/');done
