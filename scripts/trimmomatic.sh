decdb3f5e53f8854ec257e676c7e5f3ada154105
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-09-09 23:57:18 -0500

    update trimmomatic.sh
    Don't install trimmomatic, it will be installed using conda.
    Don't expect trim_loc (trimmomatic location) as input.
    Use "basename" command instead of "echo" in order to obtain output filenames.

--- scripts/trimmomatic.sh
+++ scripts/trimmomatic.sh
@@ -6,7 +6,7 @@
 set -e
 
 function usage() {
-    echo "Usage: $0 -i <input directory> -o <output directory> [-t <threads>] [--trim-dir <Trimmomatic directory>]"
+    echo "Usage: $0 -i <input directory> -o <output directory> [-t <threads>]"
     echo "Output directory will be created if it doesn't exists."
 }
 
@@ -29,9 +29,6 @@ while [[ -n "$1" ]]; do
         -t )        threads="$2"
             shift
             ;;
-        --trim-dir )    trim_loc=$(readlink -f "$2")
-            shift
-            ;;
         * )         echo "Option '$1' not recognized"; exit 1
             ;;
     esac
@@ -50,35 +47,25 @@ if [ ! -d "$out_dir" ]; then
 	mkdir -p "$out_dir"
 fi
 
-# Install Trimmomatic
-
-if [[ -z "$trim_loc" ]]; then
-    mkdir -p "$trim_loc"
-    echo 'Installing Trimmomatic...'
-    wget -nd -P "$trim_loc" http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip || exit 1
-    unzip "$trim_loc"/Trimmomatic-0.39.zip
-fi
-
-trimmomatic=$(ls "$trim_loc"/trimmomatic*.jar)
-
 read -p "Write arguments for Trimmomatic: " trimopt
 # Las opciones utilizadas con los reads de prueba el 2020-07-13
 # fueron: MINLEN:140 TRAILING:25 HEADCROP:20
 # Es importante recordar que en Trimmomatic el orden de las opciones indica
 # su orden de ejecución.
 
-for file in "$input_dir"/*R1*.fastq;
-do java -jar $trimmomatic PE -threads ${threads:=4} "$file" $(echo "$file" | sed 's/R1/R2/') \
-"$out_dir"/$(echo "$file" | sed 's/R1.*/f-paired.fq.gz/')   \
-"$out_dir"/$(echo "$file" | sed 's/R1.*/f-unpaired.fq.gz/') \
-"$out_dir"/$(echo "$file" | sed 's/R1.*/r-paired.fq.gz/')   \
-"$out_dir"/$(echo "$file" | sed 's/R1.*/r-unpaired.fq.gz/') \
+for file in "$input_dir"/*R1*.fastq; do
+trimmomatic PE -threads ${threads:=4} "$file" $(echo "$file" | sed 's/R1/R2/') \
+"$out_dir"/$(basename "$file" | sed 's/R1.*/f-paired.fq.gz/')   \
+"$out_dir"/$(basename "$file" | sed 's/R1.*/f-unpaired.fq.gz/') \
+"$out_dir"/$(basename "$file" | sed 's/R1.*/r-paired.fq.gz/')   \
+"$out_dir"/$(basename "$file" | sed 's/R1.*/r-unpaired.fq.gz/') \
 $trimopt 2>&1 | tee -a "$out_dir"/trimmomatic.log
 done
 
+### Corregir esta parte porque está borrando todos los archivos output
 # Clean up empty files
-for file in "$out_dir"/*.fq.gz; do
-    [[ ! -s file ]] && rm "$file"
-done
+#for file in "$out_dir"/*.fq.gz; do
+#    [[ ! -s file ]] && rm "$file"
+#done
 
 echo "Done. You should now execute Bowtie2 in order to clean contaminant reads."
