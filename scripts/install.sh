92e793d20db11a97c727b263d1a82ff3d94460c1
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-10-21 13:05:09 -0500

    add installation script

---
+++ scripts/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Pipeline installation script
+# Written by: Phagomica group
+# Last updated on: 2020-09-06
+
+# Create Conda environments
+echo "Creating hummann2 environment..."
+conda create --name humann2 --file ../conda-envs/humann2-env.yml
+
+echo "Creating preprocessing environment..."
+conda create --name preprocessing --file preprocessing.yml
+
+# TODO crear ambientes de conda y archivos yml
+
+conda create --name binning --file # TODO poner yml
+
+conda create --name assembly --file # TODO poner yml
+
+conda create --name metaphlan --file # TODO poner yml
+
+conda create --name picking16S --file # TODO poner yml
