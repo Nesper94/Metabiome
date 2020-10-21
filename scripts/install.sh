430fc2a91e67d5c31d1a1a2bc702978e022672ad
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-10-21 15:13:32 -0500

    replace: conda create -> conda env create

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,17 +5,17 @@
 
 # Create Conda environments
 echo "Creating hummann2 environment..."
-conda create --name humann2 --file ../conda-envs/humann2-env.yml
+conda env create --name humann2 --file ../conda-envs/humann2-env.yml
 
 echo "Creating preprocessing environment..."
-conda create --name preprocessing --file ../conda-envs/preprocessing.yml
+conda env create --name preprocessing --file ../conda-envs/preprocessing.yml
 
 # TODO crear ambientes de conda y archivos yml
 
-#conda create --name binning --file # TODO poner yml
+#conda env create --name binning --file # TODO poner yml
 
-#conda create --name assembly --file # TODO poner yml
+#conda env create --name assembly --file # TODO poner yml
 
-#conda create --name metaphlan --file # TODO poner yml
+#conda env create --name metaphlan --file # TODO poner yml
 
-#conda create --name picking16S --file # TODO poner yml
+#conda env create --name picking16S --file # TODO poner yml
