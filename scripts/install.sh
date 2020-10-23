c4174b53cd1193df43038eacdb031731875a378a
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-10-22 23:53:04 -0500

    remove --name flag when creating envs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Pipeline installation script
 # Written by: Phagomica group
-# Last updated on: 2020-09-06
+# Last updated on: 2020-10-22
 
 # Create Conda environments
 echo "Creating hummann2 environment..."
-conda env create --name humann2 --file ../conda-envs/humann2-env.yml
+conda env create --file ../conda-envs/humann2-env.yml
 
 echo "Creating preprocessing environment..."
-conda env create --name preprocessing --file ../conda-envs/preprocessing.yml
+conda env create --file ../conda-envs/preprocessing.yml
 
 # TODO crear ambientes de conda y archivos yml
 
-#conda env create --name binning --file # TODO poner yml
+#conda env create --file # TODO poner yml binning
 
-#conda env create --name assembly --file # TODO poner yml
+#conda env create --file # TODO poner yml assembly
 
-#conda env create --name metaphlan --file # TODO poner yml
+#conda env create --file # TODO poner yml metaphlan
 
-#conda env create --name picking16S --file # TODO poner yml
+#conda env create --file # TODO poner yml picking16S
