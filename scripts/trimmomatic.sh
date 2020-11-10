98038ffa5c9dbe575874be528afc3578bf4117af
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-11-09 19:24:52 -0500

    add space to usage function

--- scripts/trimmomatic.sh
+++ scripts/trimmomatic.sh
@@ -9,6 +9,7 @@ source "$SCRIPTS_DIR"/functions.sh
 
 function usage() {
     echo "Usage: $0 -i <input directory> -o <output directory> [-t <threads>] 'TRIMMOMATIC_OPTIONS'"
+    echo ""
     echo "Make sure TRIMMOMATIC_OPTIONS are enclosed within quotation marks."
     echo "Output directory will be created if it doesn't exists."
 }
