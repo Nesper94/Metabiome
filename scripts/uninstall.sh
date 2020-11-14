5566404c23e573517343498d45f3d1ebce61fffc
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-11-14 15:22:06 -0500

    ask for confirmation when uninstalling

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -4,20 +4,31 @@
 # Last updated on: 2020-10-22
 
 # Remove Conda environments
-echo "Removing hummann2 environment..."
-conda env remove --name hummann2
+remove_envs(){
+  echo "Removing hummann2 environment..."
+  conda env remove --name hummann2
 
-echo "Removing preprocessing environment..."
-conda env remove --name preprocessing
+  echo "Removing preprocessing environment..."
+  conda env remove --name preprocessing
 
-echo "Removing binning environment..."
-conda env remove --name binning
+  echo "Removing binning environment..."
+  conda env remove --name binning
 
-echo "Removing assembly environment..."
-conda env remove --name assembly
+  echo "Removing assembly environment..."
+  conda env remove --name assembly
 
-echo "Removing metaphlan environment..."
-conda env remove --name metaphlan
+  echo "Removing metaphlan environment..."
+  conda env remove --name metaphlan
 
-echo "Removing picking16S environment..."
-conda env remove --name picking16S
+  echo "Removing picking16S environment..."
+  conda env remove --name picking16S
+}
+
+while true; do
+  read -p "Are you sure to uninstall Metabiome? [y/n] " answer
+  case "$answer" in
+    [Yy]* ) remove_envs; break;;
+    [Nn]* ) exit;;
+    * )     echo "Please answer yes or no.";;
+  esac
+done
