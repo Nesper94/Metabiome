acdc42fdff69b1a84bbcca9d7d81efeed8f47cfd
Estefany-Lorenzana <estefany.lorenzana@udea.edu.co>
2020-11-26 23:39:12 -0500

    restore files to delete them from git history

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 COMPLETION_DIR=${BASH_COMPLETION_USER_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion}/completions
+AT_INSTALL_COMPLETION_DIR=/home/estefany/.local/share/bash-completion/completions
