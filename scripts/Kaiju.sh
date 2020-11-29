116482817c1b931b6ef146add8ce3c85a802e2d6
biocdgv@gmail.com <biocdgv@gmail.com>
2020-11-28 20:25:21 -0500

    Style the usage function and iterators
    The change affects:
    -BBduk.sh
    -Kaiju.sh
    -MetaPhlAn3.sh
    -bowtie2.sh
    
    The change includes:
    -Organize the usage function's output.
    -Change the implicit iterators from the loop to more explicit ones.
    -Organize the script to be more human-readable.
    -Check if the scripts work after the changes.

--- scripts/Kaiju.sh
+++ scripts/Kaiju.sh
@@ -9,22 +9,23 @@ SCRIPTS_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 source "$SCRIPTS_DIR"/functions.sh
 
 function usage() {
-    echo "Usage: metabiome kaiju -i <input directory> -o <output directory> -D <database_directory> -d <name_kaiju_database> [-t <threads>] [-kr <kraken_input> ] [-k <krona_dir> ] [-c <class_dir> ] [-x <taxa_names_dir> ] [-m <merge_dir> ] ['kaiju_OPTIONS']"
+    echo "Usage: metabiome kaiju [options] -i <in_dir> -o <out_dir> -D <db_dir> -d <kaiju_db>"
     echo ""
+    echo "Mandatory: "
+    echo "-i, <in_dir>              Input directory containing FASTQ files."
+    echo "-o, <out_dir>             Directory in which results will be saved. This directory"
+    echo "                          will be created if it doesn't exist."
+    echo "-D, <db_dir>              Kaiju Database directory."
+    echo "-d, <kaiju_db>            Kaiju Database name. Please refer to the Kaiju github repository"
     echo "Options:"
-    echo "<input_directory>  Input directory containing clean FASTQ files."
-    echo "<output_directory> Directory in which results will be saved. This directory"
-    echo "will be created if it doesn't exist."
-    echo "<database_directory>    Kaiju Database directory."
-    echo "<name_kaiju_database>   Kaiju Database name. Please refer to the Kaiju"
-    echo "github repository in order to know the names"
-    echo "<threads> Number of threads to use (default=1)."
-    echo "<class>        Directory where to put the classification summary of Kaiju's output. (optional)"
-    echo "<taxa_names>   Directory where to put the taxa names from the Kaiju's output file. (optional)"
-    echo "<merge>        Directory where to put the merging outputs from Kaiju and Kraken. (optional)"
-    echo "<krona_dir>    Directory where to put Krona output. (optional)"
-    echo "<kraken_input> Kraken output files's directory. (optional)"
-    echo "<kaiju_OPTIONS> Kaiju's options (optional). Make sure to enclose Kaiju_OPTIONS within quotation marks"
+    echo "-t, <threads>             Number of threads to use (default=1)."
+    echo "-c, <class_dir>           Directory of the classification summary of Kaiju's output. (optional)"
+    echo "-x, <taxa_dir>            Directory of the taxa names from the Kaiju's output file. (optional)"
+    echo "-m, <merge_dir>           Directory of the merging outputs from Kaiju and Kraken. (optional)"
+    echo "-k, <krona_dir>           Directory of the Krona output. (optional)"
+    echo "-kr, <kraken_in>          Kraken output files's directory. (optional)"
+    echo "-opts, <kaiju_OPTIONS>    Kaiju's options. (optional)"
+    echo "Please execute '(kaiju -h)' in order to see bowtie2's documentation "
 }
 
 ##--------------------------Exiting if input files are missing---------------##:
@@ -33,44 +34,20 @@ validate_arguments "$#"
 ##------------------Saving input orders into variables-----------------------##:
 while [[ -n "$1" ]]; do
     case "$1" in
-        -h|--help ) usage; exit 0
-            ;;
-        -i )        input_dir=$(readlink -f "$2")
-            shift
-            ;;
-        -o )        out_dir=$(readlink -f "$2")
-            shift
-            ;;
-        -D )        database=$(readlink -f "$2")
-            shift
-            ;;
-        -d )        name_kaijudb="$2"
-            shift
-            ;;
-        -t )        threads="$2"
-            shift
-            ;;
-        -c )        class=$(readlink -f "$2")
-           shift
-            ;;
-        -x )        taxa_names=$(readlink -f "$2")
-           shift
-            ;;
-        -m )        merge=$(readlink -f "$2")
-           shift
-            ;;
-        -k )        krona=$(readlink -f "$2")
-           shift
-            ;;
-        -kr )       kraken=$(readlink -f "$2")
-           shift
-            ;;
-        * )         kaiju_opts="$@"
-            ;;
-        * )        echo "Option '$1' not recognized"; exit 1
-            ;;
+        -h|--help ) usage; exit 0 ;;
+        -i )        input_dir=$(readlink -f "$2"); shift 2 ;;
+        -o )        out_dir=$(readlink -f "$2"); shift 2 ;;
+        -D )        database=$(readlink -f "$2"); shift 2 ;;
+        -d )        name_kaijudb="$2"; shift 2 ;;
+        -t )        threads="$2"; shift 2 ;;
+        -c )        class=$(readlink -f "$2"); shift 2 ;;
+        -x )        taxa_names=$(readlink -f "$2"); shift 2 ;;
+        -m )        merge=$(readlink -f "$2"); shift 2 ;;
+        -k )        krona=$(readlink -f "$2"); shift 2 ;;
+       -kr )        kraken=$(readlink -f "$2"); shift 2 ;;
+     -opts )        shift; kaiju_opts="$@"; break ;;
+        * )         echo "Option '$1' not recognized"; exit 1 ;;
     esac
-    shift
 done
 
 ##----------------Verify that input directory exists-------------------------##:
@@ -97,18 +74,18 @@ echo "Kaiju version: $(kaiju -h)"
 cd "$database";kaiju-makedb -s "$name_kaijudb" -t "$threads"; }
 
 ##-----------------------Paired-end reads------------------------------------##:
-for i in "$input_dir"/*1_paired_bt2.fq.gz;do
-  kaiju -t "$database"/*nodes.dmp -f "$database"/"$name_kaijudb"/*.fmi -i "$i" \
-  -j $(echo $i | sed 's/1_paired/2_paired/') \
-  -o  "$out_dir"/$(echo $(basename -- $i) | sed 's/1_paired_bt2.fq.gz/paired_kaiju.txt/') \
-  -z "$threads" "${kaiju_opts:=''}"
+for forward_file in "$input_dir"/*1_paired*;do
+  kaiju -t "$database"/*nodes.dmp -f "$database"/"$name_kaijudb"/*.fmi -i "$forward_file" \
+  -j $(echo "$forward_file" | sed 's/1_paired/2_paired/') \
+  -o  "$out_dir"/$(echo $(basename -- "$forward_file") | sed 's/1_paired_bt2.fq.gz/paired_kaiju.txt/') \
+  -z "$threads" "$kaiju_opts"
 done
 
 ##-----------------------Single-end reads------------------------------------##:
-for i in "$input_dir"/*unpaired_bt2.fq.gz;do
-  kaiju -t "$database"/*nodes.dmp -f "$database"/"$name_kaijudb"/*.fmi -i "$i" \
-  -o "$out_dir"/$(echo $(basename -- $i) | sed 's/unpaired_bt2.fq.gz/unpaired_kaiju.txt/') \
-  -z "$threads" "${kaiju_opts:=''}"
+for forward_file in "$input_dir"/*_unpaired_*;do
+  kaiju -t "$database"/*nodes.dmp -f "$database"/"$name_kaijudb"/*.fmi -i "$forward_file" \
+  -o "$out_dir"/$(echo $(basename -- "$forward_file" ) | sed 's/unpaired_bt2.fq.gz/unpaired_kaiju.txt/') \
+  -z "$threads" "$kaiju_opts"
 done
 
 
@@ -116,35 +93,39 @@ done
 ##-----------------------Kaiju to Krona--------------------------------------##:
 if [ -d "$krona" ]; then
   cd "$krona" && mkdir html && cd ..
-    for i in "$out_dir"/*kaiju.txt;do
+    for kaiju_out in "$out_dir"/*kaiju.txt;do
           kaiju2krona -t "$database"/nodes.dmp  -n "$database"/names.dmp \
-          -i $i -o "$krona"/$(echo $(basename -- $i) | sed 's/_kaiju.txt/.krona/');done
-    for i in "$krona"/*.txt;do
-          ktImportText $i -o "$krona"/html/$(echo $(basename -- $i) | sed 's/.krona/krona.html/');done
+          -i "$kaiju_out" -o "$krona"/$(echo $(basename -- "$kaiju_out") | sed 's/_kaiju.txt/.krona/')
+    done
+    for krona_out in "$krona"/*.txt;do
+          ktImportText "$krona_out" -o "$krona"/html/$(echo $(basename -- "$krona_out") | sed 's/.krona/krona.html/')
+    done
 fi
 ##-------------------Classification summary from Kaiju's output----------------------##:
 if [ -d "$class" ]; then
-  for i in "$out_dir"/*kaiju.txt;do
-        kaiju2table $i -t "$database"/nodes.dmp  -n "$database"/names.dmp \
-        -p -m 1.0 -r genus -o "$class"/$(echo $(basename -- $i) | sed 's/kaiju.txt/classif.txt/');done
+  for kaiju_out in "$out_dir"/*kaiju.txt;do
+        kaiju2table "$kaiju_out" -t "$database"/nodes.dmp  -n "$database"/names.dmp \
+        -p -m 1.0 -r genus -o "$class"/$(echo $(basename -- "$kaiju_out") | sed 's/kaiju.txt/classif.txt/')
+  done
 fi
 ##------------------Adding taxa names to output file-------------------------##:
 if [ -d "$taxa_names" ]; then
-      for i in "$out_dir"/*kaiju.txt;do
+  for kaiju_out in "$out_dir"/*kaiju.txt;do
         kaiju-addTaxonNames -t "$database"/nodes.dmp -p \
-        -n "$database"/names.dmp -i $i \
-        -o "$taxa_names"/$(echo $(basename -- $i) | sed 's/kaiju.txt/tax_names.txt/');done
+        -n "$database"/names.dmp -i "$kaiju_out" \
+        -o "$taxa_names"/$(echo $(basename -- "$kaiju_out") | sed 's/kaiju.txt/tax_names.txt/')
+  done
 fi
 ##---------------Merging outputs from kraken and Kaiju-----------------------##:
 if [[ -d "$merge" ]] && [[ -d "$kraken"]] ; then
-    for i in "$out_dir"/*kaiju.txt;do
-          for j in "$kraken"/*.fq;do
+    for kaiju_out in "$out_dir"/*kaiju.txt;do
+          for kraken_out in "$kraken"/*.fq;do
                   echo "sorting kaiju and kraken output files"
-                  sort -k2,2 $i > $(echo $(basename -- $i) | sed 's/kaiju.txt/kaiju.mrgsrt/')
-                  sort -k2,2 $j > $(echo $(basename -- $j) | sed 's/.fq/kraken.mrgsrt/')
+                  sort -k2,2 "$kaiju_out" > $(echo $(basename -- "$kaiju_out" ) | sed 's/kaiju.txt/kaiju.mrgsrt/')
+                  sort -k2,2 "$kraken_out" > $(echo $(basename -- "$kraken_out" ) | sed 's/.fq/kraken.mrgsrt/')
                   kaiju-mergeOutputs  -i *kaiju.mrgsrt -j *kraken.mrgsrt \
                   -t "$database"/nodes.dmp -c lowest \
-                  -o "$merge"/$(echo $(basename -- $i) | sed 's/kaiju.txt/merged.txt/') -v
+                  -o "$merge"/$(echo $(basename -- "$kaiju_out") | sed 's/kaiju.txt/merged.txt/') -v
                   rm *.mrgsrt
           done
     done
--- scripts/bowtie2.sh
+++ scripts/bowtie2.sh
@@ -9,18 +9,20 @@ SCRIPTS_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 source "$SCRIPTS_DIR"/functions.sh
 
 function usage() {
-    echo "Usage: metabiome bowtie2 -i <in dir> -o <out dir> -ho <host> [-ph <PhiX>] [-hu <human>] [-t <threads>] [index_OPTIONS] [bowtie2_OPTIONS]"
+    echo "Usage: metabiome bowtie2 [options] -i <in_dir> -o <out_dir>"
     echo ""
+    echo "Mandatory: "
+    echo "-i, <in_dir>               Input directory containing FASTQ files."
+    echo "-o, <out_dir>              Directory in which results will be saved. This directory"
+    echo "                           will be created if it doesn't exist."
     echo "Options:"
-    echo "<in_dir>  Input directory containing FASTQ files."
-    echo "<out_dir> Directory in which results will be saved. This directory"
-    echo "will be created if it doesn't exist."
-    echo "<host>    Host reference genome in FASTA format. (optional)"
-    echo "<PhiX>    PhiX-174 phage reference genome in FASTA format. (optional)"
-    echo "<human>   Human reference genome in FASTA format. (optional)"
-    echo "<threads> Number of threads to use. (default=1)"
-    echo "<index_OPTIONS> bowtie2 index builder's options (optional). Make sure to enclose index_OPTIONS within quotation marks"
-    echo "<bowtie2_OPTIONS> bowtie2's options (optional). Make sure to enclose bowtie2_OPTIONS within quotation marks"
+    echo "-ho, <host>                Host reference genome in FASTA format. (optional)"
+    echo "-ph, <PhiX>                PhiX-174 phage reference genome in FASTA format. (optional)"
+    echo "-hu, <human>               Human reference genome in FASTA format. (optional)"
+    echo "-t,  <threads>             Number of threads to use. (default=1)"
+    echo "-idx, <index_OPTIONS>      bowtie2 index builder's options (optional)."
+    echo "-opts, <bowtie2_OPTIONS>   bowtie2's options (optional)."
+    echo "Please execute '(bowtie2 --help)' in order to see bowtie2's documentation "
 
 }
 
@@ -28,36 +30,20 @@ function usage() {
 validate_arguments "$#"
 
 ##---------------------Save input parameters into variables------------------##:
+
 while [[ -n "$1" ]]; do
     case "$1" in
-        -h|--help ) usage; exit 0
-            ;;
-        -i )        input_dir=$(readlink -f "$2")
-            shift
-            ;;
-        -o )        out_dir=$(readlink -f "$2")
-            shift
-            ;;
-        -ho )       host=$(readlink -f "$2")
-            shift
-            ;;
-        -t )        threads="$2"
-           shift
-            ;;
-        -ph )       PhiX="$2"
-           shift
-            ;;
-        -hu )       Human="$2"
-           shift
-            ;;
-        * )         index_opts="$@"
-            ;;
-        * )         bowtie2_opts="$@"
-            ;;
-        * )        echo "Option '$1' not recognized"; exit 1
-            ;;
+        -h|--help ) usage; exit 0 ;;
+        -i )        input_dir=$(readlink -f "$2"); shift 2 ;;
+        -o )        out_dir=$(readlink -f "$2"); shift 2 ;;
+        -ho )       host=$(readlink -f "$2"); shift 2 ;;
+        -t )        threads="$2"; shift 2 ;;
+        -ph )       PhiX="$2"; shift 2 ;;
+        -hu )       Human="$2"; shift 2 ;;
+        -idx )      shift; index_opts="$@";;
+        -opts )     shift; bowtie2_opts="$@"; break ;;
+        * )        echo "Option '$1' not recognized"; exit 1 ;;
     esac
-    shift
 done
 
 ##----------------------Activate Conda environment-----------------------------##:
@@ -68,33 +54,42 @@ activate_env metabiome-preprocessing
 ##were successfull.
 
 for i in {1..10}; do
-
     if [ -e "$Human" ]; then
       echo "Human Reference Genome already downloaded"
-    else
-      echo "Downloading Human Reference Genome"
-      esearch -db nucleotide -query "GCA_000001405.28" | \
-      efetch -format fasta > GCA_000001405.28.fasta
-      if  [ -e GCA_000001405.28.fasta ]; then
-          echo "Human genome was downloaded"
-          Human=$(readlink -f GCA_000001405.28.fasta)
-      fi
-    fi
-
-    if [ -e "$PhiX" ]; then
-      echo "PhiX Genome already downloaded"
       break
     else
-      echo "Downloading PhiX Reference Genome"
+      echo "Downloading Human Reference Genome"
       esearch -db nucleotide -query "NC_001422.1" | \
-      efetch -format fasta > NC_001422.1.fasta
-      if  [ -e NC_001422.1.fasta ]; then
-        echo "PhiX genome was downloaded"
-        PhiX=$(readlink -f NC_001422.1.fasta)
-        break
+      efetch -format fasta > Human_GCA_000001405.28.fasta
+      if  [[ -e Human_GCA_000001405.28.fasta ]] && [[ -s Human_GCA_000001405.28.fasta ]]; then
+          echo "Human genome was downloaded"
+          Human=$(readlink -f Human_GCA_000001405.28.fasta)
+          break
+      else
+        echo "Try again downloading the human genome"
       fi
     fi
 done
+
+for i in {1..10}; do
+  if [ -e "$PhiX" ]; then
+    echo "PhiX Genome already downloaded"
+    break
+  else
+    echo "Downloading PhiX Reference Genome"
+    esearch -db nucleotide -query "NC_001422.1" | \
+    efetch -format fasta > NC_001422.1.fasta
+    if  [[ -e NC_001422.1.fasta ]] && [[ -s NC_001422.1.fasta ]]; then
+      echo "PhiX genome was downloaded"
+      PhiX=$(readlink -f NC_001422.1.fasta)
+      break
+    else
+      echo "Try again downloading the PhiX genome"
+    fi
+  fi
+done
+
+
 
 # Verify that input directory is set and exists
 validate_input_dir
@@ -107,10 +102,10 @@ echo "Conda environment: $CONDA_DEFAULT_ENV"
 echo "Input directory: ${input_dir}"
 echo "Output directory: ${out_dir}"
 echo "Number of threads: ${threads:=1}"
-echo "Host Genome: ${host:?'=Host genome not set'}"
 echo "Phix Genome: ${PhiX:?'=PhiX genome not set'}"
 echo "Human Genome: ${Human:?'=Human genome not set'}"
-echo "Bowtie2 version: $(bowtie2  --version)"
+echo "Bowtie2 called with options: $bowtie2_opts"
+
 
 ##------------Concatenate genomes to be aligned and build genome index-------##:
 ##First checks if the index is already generated, otherwise it will be generated.
@@ -123,32 +118,35 @@ fi
 dir=$(pwd) ##current directory
 [ ! -f "$dir"/Mix.3.bt2 ] \
 && { echo "Index needs to be generated:";
-    bowtie2-build Mixed.fasta Mix --threads "$threads" "${index_opts:=''}";}
+    bowtie2-build Mixed.fasta Mix --threads "$threads" "$index_opts";}
 
 ##--------------------------Pair end (PE) alignment--------------------------##:
-for i in "$input_dir"/*1_paired_trim.fq.gz; do
+
+for forward_file in "$input_dir"/*1_paired_* ; do
     echo "Performing paired reads alignment..."
-    bowtie2 -x Mix -1 $i -2 $(echo $i | sed 's/1_paired_trim/2_paired_trim/') \
-    --un-conc-gz "$out_dir"/$(echo $(basename -- $i) | sed 's/1_paired_trim/paired_bt2/') \
-    -q -p $threads 2> "$out_dir"/$(echo $(basename -- $i) | sed 's/1_paired_trim.fq.gz/paired_bt2_summary.txt/') \
-    "${bowtie2_opts:=''}"
-    > /dev/null # Bowtie2 output to terminal is excesive and we don't need it in this case
+    bowtie2 -x Mix -1 "$forward_file" -2 $(echo "$forward_file" | sed 's/_1_paired_/_2_paired_/') \
+    --un-conc-gz "$out_dir"/$(echo $(basename -- "$forward_file" ) | sed 's/1_paired_trim/paired_bt2/') \
+    -q -p "$threads" 2> "$out_dir"/$(echo $(basename -- "$forward_file" ) | sed 's/1_paired_trim.fq.gz/paired_bt2_summary.txt/') \
+    "$bowtie2_opts" \
+    > /dev/null # Bowtie2 output to terminal is excesive and we do not need it in this case
 done
 
 ##-------------------------Single end (SE) alignment------------------------##:
-
-for i in "$input_dir"/*unpaired_trim.fq.gz; do
+for unpaired_file in "$input_dir"/*_unpaired_* ; do
     echo "Performing single reads alignment..."
-    bowtie2 -x Mix -U $i --un-gz "$out_dir"/$(echo $(basename -- $i) | sed 's/unpaired_trim/unpaired_bt2/') \
-    -q -p $threads 2> "$out_dir"/$(echo $(basename -- $i) | sed 's/unpaired_trim.fq.gz/unpaired_bt2_summary.txt/') \
-    "${bowtie2_opts:=''}"
+    bowtie2 -x Mix -U "$unpaired_file" --un-gz "$out_dir"/$(echo $(basename -- "$unpaired_file" ) | sed 's/unpaired_trim/unpaired_bt2/') \
+    -q -p "$threads" 2> "$out_dir"/$(echo $(basename -- "$unpaired_file" ) | sed 's/unpaired_trim.fq.gz/unpaired_bt2_summary.txt/') \
+    "$bowtie2_opts" \
     > /dev/null
 done
 
 ##---------------- Rename files according to the naming convention-----------##:
 cd "$out_dir"
-rename 's/paired_bt2.fq.1.gz/1_paired_bt2.fq.gz/' *paired_bt2.fq.1.gz
-rename 's/paired_bt2.fq.2.gz/2_paired_bt2.fq.gz/' *paired_bt2.fq.2.gz
+##renaming paired-end reads:
+for i in {1..2}; do
+  rename 's/paired_bt2.fq.'"$i"'.gz/'"$i"'_paired_bt2.fq.gz/' *paired_bt2.fq."$i".gz
+done
+
 
 echo "Done."
 echo "You can now use clean reads to:"
