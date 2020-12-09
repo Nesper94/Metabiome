a96ca9a3c40d951a1acd106c5fdb74c9378d65aa
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-12-09 00:33:14 -0500

    fix: Invoke scripts using bash
    This removes the requirement that scripts have execution
    permissions and forces the scripts to be executed with bash.

--- scripts/metabiome.sh
+++ scripts/metabiome.sh
@@ -28,31 +28,31 @@ validate_arguments "$#"
 
 # Get input arguments
 if [[ "$1" = "bbduk" ]]; then
-  "$SCRIPTS_DIR"/BBduk.sh ${@:2}
+  bash "$SCRIPTS_DIR"/BBduk.sh ${@:2}
 
 elif [[ "$1" = "bowtie2" ]]; then
-  "$SCRIPTS_DIR"/bowtie2.sh ${@:2}
+  bash "$SCRIPTS_DIR"/bowtie2.sh ${@:2}
 
 elif [[ "$1" = "humann2" ]]; then
-  "$SCRIPTS_DIR"/humann2.sh ${@:2}
+  bash "$SCRIPTS_DIR"/humann2.sh ${@:2}
 
 elif [[ "$1" = "kaiju" ]]; then
-  "$SCRIPTS_DIR"/Kaiju.sh ${@:2}
+  bash "$SCRIPTS_DIR"/Kaiju.sh ${@:2}
 
 elif [[ "$1" = "kraken2" ]]; then
-  "$SCRIPTS_DIR"/kraken2.sh ${@:2}
+  bash "$SCRIPTS_DIR"/kraken2.sh ${@:2}
 
 elif [[ "$1" = "megahit" ]]; then
-  "$SCRIPTS_DIR"/megahit.sh ${@:2}
+  bash "$SCRIPTS_DIR"/megahit.sh ${@:2}
 
 elif [[ "$1" = "metaphlan3" ]]; then
-  "$SCRIPTS_DIR"/MetaPhlAn3.sh ${@:2}
+  bash "$SCRIPTS_DIR"/MetaPhlAn3.sh ${@:2}
 
 elif [[ "$1" = "metaspades" ]]; then
-  "$SCRIPTS_DIR"/metaspades.sh ${@:2}
+  bash "$SCRIPTS_DIR"/metaspades.sh ${@:2}
 
 elif [[ "$1" = "trimmomatic" ]]; then
-  "$SCRIPTS_DIR"/trimmomatic.sh ${@:2}
+  bash "$SCRIPTS_DIR"/trimmomatic.sh ${@:2}
 
 elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]; then
   usage
