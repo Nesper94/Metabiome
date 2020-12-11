490e8e57f330f20365f82f2f507a12ab9a889f38
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-12-11 00:33:35 -0500

    Implement new naming convention in MetaPhlAn3.sh

--- scripts/MetaPhlAn3.sh
+++ scripts/MetaPhlAn3.sh
@@ -19,7 +19,7 @@ function usage() {
     echo ""
     echo "Options:"
     echo "  -t NUM                    Number of threads to use. (default=1)"
-    echo "  -opts metaphlan_OPTIONS   MetaPhlAn3's options."
+    echo "  -opts MetaPhlAn_OPTIONS   MetaPhlAn3's options."
     echo "  -h, --help                Show this help"
 }
 
@@ -47,8 +47,8 @@ validate_output_dir
 ##----------------Activate conda environment--------------------------------##:
 activate_env metabiome-metaphlan
 
-##----------------Installing MetaPhlAn database-----------------------------##:
-##First checks if MetaPhlAn3 database already generated, otherwise it will be generated.
+##--------------------Install MetaPhlAn database-----------------------------##:
+# First checks if MetaPhlAn3 database already generated, otherwise it will be generated.
 [ ! -f "$met_database"/mpa_v30_CHOCOPhlAn_201901.4.bt2 ] \
 && { echo "MetaPhlAn3 database needs to be downloaded" ; \
      metaphlan --install --bowtie2db "$met_database"; }
@@ -62,27 +62,30 @@ echo "MetaPhlAn3 database: ${met_database:?'Database not downloaded'}"
 echo "MetaPhlAn3 version: $(metaphlan -v)"
 
 ##---------------------------MetaPhlAn profiling----------------------------##:
-##----------------------PE reads--------------------------------------------##:
-for foward_file in "$input_dir"/*_1_paired_;do
-  metaphlan "$forward_file",$(echo "$forward_file" | sed 's/_1_/_2_/') \
-  --input_type fastq  -t rel_ab_w_read_stats \
-  -o "$out_dir"/$(echo $(basename -- "$forward_file") | sed 's/1_paired_bt2.fq.gz/paired_mphlan.txt/') \
-  --nproc "$threads" --bowtie2out "$out_dir"/$(echo $(basename -- "$forward_file") | sed 's/1_paired_bt2.fq.gz/paired_bt2_mphlan.sam/') \
-  "$metaphlan_opts"
+for file in "$input_dir"/*; do
+##-----------------------------Paired End reads------------------------------##:
+    if [[ "$file" == @(*_R1_*|*_1).@(fastq|fq.gz|fastq.gz) ]]; then
+        forward_file="$file"
+        metaphlan "$forward_file",$(echo "$forward_file" | forward_to_reverse) \
+        --input_type fastq -t rel_ab_w_read_stats \
+        -o "$out_dir"/$(get_core_name "$forward_file" | sed 's/_bt2//')_mphlan.txt \
+        --nproc "$threads" \
+        --bowtie2out "$out_dir"/$(get_core_name "$forward_file" | sed 's/_bt2//')_mphlan.sam \
+        "$metaphlan_opts"
+##------------------------------Single End reads-----------------------------##:
+    elif [[ "$file" == *_unpaired_* ]]; then
+        unpaired_file="$file"
+        metaphlan "$unpaired_file" --input_type fastq -t rel_ab_w_read_stats \
+        -o "$out_dir"/$(get_core_name "$unpaired_file" | sed 's/_bt2//')_mphlan.txt \
+        --nproc "$threads" \
+        --bowtie2out "$out_dir"/$(get_core_name "$unpaired_file" | sed 's/_bt2//')_mphlan.sam \
+        "$metaphlan_opts"
+    fi
 done
 
-##------------------------------SE reads------------------------------------##:
-for unpaired_file in "$input_dir"/*_unpaired_;do
-  metaphlan "$unpaired_file" --input_type fastq -t rel_ab_w_read_stats \
-  -o "$out_dir"/$(echo $(basename -- "$unpaired_file") | sed 's/unpaired_bt2.fq.gz/unpaired_mphlan.txt/') \
-  --nproc "$threads" --bowtie2out "$out_dir"/$(echo $(basename -- "$unpaired_file") | sed 's/unpaired_bt2.fq.gz/un_bt2_mphlan.sam/') \
-  "$metaphlan_opts"
-done
-
-##-----------------------------Merging tables from utility scripts-----------##:
+##-----------------------Merge tables from utility scripts-------------------##:
 cd "$out_dir"
 merge_metaphlan_tables.py *.txt > merged_mphlan.txt
-
 
 echo "Done."
 echo "You can now use this metagenomic profiling to :"
