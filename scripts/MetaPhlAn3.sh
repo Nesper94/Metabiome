5cef4b5d4f99c8fc6c8685683515ab435e8baa6c
biocdgv@gmail.com <biocdgv@gmail.com>
2021-01-25 18:24:04 -0500

    Fix naming convention in MetaPhlAn3 script
    -Set MetaPhlAn3 script to fit new naming convention of paired-end and single-end reads.
    -Re-organize the loops of the taxonomic profiling of paired-end and single-end reads.
    -Change name environment from metabiome-metaphlan to metabiome-taxonomic-profiling.

--- scripts/MetaPhlAn3.sh
+++ scripts/MetaPhlAn3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MetaPhlAn3 wrapper script for the taxonomic profiling of reads.
 # Written by: Phagomica Group
-# Last updated on: 2020-10-28
+# Last updated on: 2021-01-25
 
 ##------------------------------Checking the input---------------------------##:
 set -e
@@ -11,16 +11,15 @@ source "$SCRIPTS_DIR"/functions.sh
 function usage() {
     echo "Usage: metabiome metaphlan3 -i <in_dir> -o <out_dir> -d <db_dir>"
     echo ""
-    echo "Mandatory:"
-    echo "  -i in_dir                 Input directory containing clean FASTQ files."
-    echo "  -o out_dir                Directory in which results will be saved. This directory"
-    echo "                            will be created if it doesn't exist."
-    echo "  -d db_dir                 MetaPhlAn3 Database directory."
-    echo ""
+    echo "Required: "
+    echo "  -i,    in_dir                 Input directory containing FASTQ reads files."
+    echo "  -o,    out_dir                Directory in which results will be saved. This directory"
+    echo "                                will be created if it doesn't exist."
+    echo "  -d,    db_dir                 MetaPhlan3 Database directory."
     echo "Options:"
-    echo "  -t NUM                    Number of threads to use. (default=1)"
-    echo "  -opts MetaPhlAn_OPTIONS   MetaPhlAn3's options."
-    echo "  -h, --help                Show this help"
+    echo "  -t,    NUM                    Number of threads to use. (default=1)"
+    echo "  -opts, OPTIONS                metaphlan's options."
+    echo "  -h,    --help                 Show this help"
 }
 
 ##--------------------------Exiting if input files are missing---------------##:
@@ -32,7 +31,7 @@ while [[ -n "$1" ]]; do
     case "$1" in
         -h|--help ) usage; exit 0;;
         -i )        input_dir=$(readlink -f "$2"); shift 2;;
-        -o )        out_dir=$(readlink -m "$2");shift 2;;
+        -o )        out_dir=$(readlink -f "$2");shift 2;;
         -d )        met_database=$(readlink -f "$2"); shift 2;;
         -t )        threads="$2"; shift 2;;
         -opts )     shift; metaphlan_opts="$@"; break ;;
@@ -45,10 +44,10 @@ validate_input_dir
 ##---------------Create output directory if it doesn't exists---------------##:
 validate_output_dir
 ##----------------Activate conda environment--------------------------------##:
-activate_env metabiome-metaphlan
+activate_env metabiome-taxonomic-profiling
 
-##--------------------Install MetaPhlAn database-----------------------------##:
-# First checks if MetaPhlAn3 database already generated, otherwise it will be generated.
+##----------------Installing MetaPhlAn database-----------------------------##:
+##First checks if MetaPhlAn3 database already generated, otherwise it will be generated.
 [ ! -f "$met_database"/mpa_v30_CHOCOPhlAn_201901.4.bt2 ] \
 && { echo "MetaPhlAn3 database needs to be downloaded" ; \
      metaphlan --install --bowtie2db "$met_database"; }
@@ -62,31 +61,39 @@ echo "MetaPhlAn3 database: ${met_database:?'Database not downloaded'}"
 echo "MetaPhlAn3 version: $(metaphlan -v)"
 
 ##---------------------------MetaPhlAn profiling----------------------------##:
+
 for file in "$input_dir"/*; do
-##-----------------------------Paired End reads------------------------------##:
-    if [[ "$file" == @(*_R1_*|*_1).@(fastq|fq.gz|fastq.gz) ]]; then
+    # Paired end reads
+    if [[ "$file" == @(*_R1_*|*_1).@(fq|fastq|fq.gz|fastq.gz) ]]; then
         forward_file="$file"
-        metaphlan "$forward_file",$(echo "$forward_file" | forward_to_reverse) \
-            --input_type fastq -t rel_ab_w_read_stats \
-            -o "$out_dir"/$(get_core_name "$forward_file" | sed 's/_bt2//')_mphlan.txt \
-            --nproc "$threads" \
-            --bowtie2out "$out_dir"/$(get_core_name "$forward_file" | sed 's/_bt2//')_mphlan.sam \
-            "$metaphlan_opts"
-##------------------------------Single End reads-----------------------------##:
-    elif [[ "$file" == *_unpaired_* ]]; then
+        core_name=$(get_core_name "$forward_file")
+        metaphlan "$forward_file",$(forward_to_reverse "$forward_file") \
+        --input_type fastq  -t rel_ab_w_read_stats \
+        -o "$out_dir"/$(echo "$core_name" | sed 's/_bt2/_mphlan/').txt \
+        --nproc "$threads" --bowtie2out "$out_dir"/$(echo "$core_name" | sed 's/_bt2/_mphlan/').sam \
+        $metaphlan_opts
+
+    # Single end reads
+    elif [[ ! "$file" ==  *_@(*R1_*|*1.|*R2_*|*2.)* ]] && [[ "$file" == *.@(fq|fastq|fq.gz|fastq.gz) ]]; then
         unpaired_file="$file"
+        core_name=$(get_core_name "$unpaired_file")
         metaphlan "$unpaired_file" --input_type fastq -t rel_ab_w_read_stats \
-            -o "$out_dir"/$(get_core_name "$unpaired_file" | sed 's/_bt2//')_mphlan.txt \
-            --nproc "$threads" \
-            --bowtie2out "$out_dir"/$(get_core_name "$unpaired_file" | sed 's/_bt2//')_mphlan.sam \
-            "$metaphlan_opts"
+        -o "$out_dir"/$(echo "$core_name" | sed 's/_bt2/_mphlan/').txt \
+        --nproc "$threads" --bowtie2out "$out_dir"/$(echo "$core_name" | sed 's/_bt2/unpaired_mphlan/').sam \
+        $metaphlan_opts
+
+    #Files that do not match the required extension:
+    elif [[ ! "$file" == *.@(fq|fastq|fq.gz|fastq.gz) ]]; then
+        echo -e "$(basename -- "$file") will not be processed as is not a .fastq or .fq.gz file."
     fi
+
 done
 
-##-----------------------Merge tables from utility scripts-------------------##:
+##-----------------------------Merging tables from utility scripts-----------##:
 cd "$out_dir"
 merge_metaphlan_tables.py *.txt > merged_mphlan.txt
 
+
 echo "Done."
-echo "You can now use this metagenomic profiling to:"
+echo "You can now use this metagenomic profiling to :"
 echo "- Play around in R or Python"
