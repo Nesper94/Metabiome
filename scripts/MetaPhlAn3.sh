4b26d98ca677573ca9c84362b6ab7e161525c1f7
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2021-02-07 19:36:14 -0500

    Make style corrections

--- scripts/MetaPhlAn3.sh
+++ scripts/MetaPhlAn3.sh
@@ -8,29 +8,29 @@ SCRIPTS_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 source "$SCRIPTS_DIR"/functions.sh
 
 function usage() {
-    cat <<HELP_USAGE
+cat <<HELP_USAGE
+Generate taxonomic profiling bins with MetaPhlAn3.
+Usage: metabiome metaphlan3 -i <in_dir> -o <out_dir> -opts metaphlan3_options
 
-    Generate taxonomic profiling bins with MetaPhlAn3.
-    Usage: metabiome metaphlan3 -i <in_dir> -o <out_dir> -opts metaphlan3_options
+Required:
+-i    in_dir      Input directory containing FASTQ reads files.
+-o    out_dir     Directory in which results will be saved. This directory
+                  will be created if it doesn't exist.
 
-    Required:
-    -i    in_dir                 Input directory containing FASTQ reads files.
-    -o    out_dir                Directory in which results will be saved. This directory
-                                 will be created if it doesn't exist.
-
-    Options:
-    -d    db_dir                 Directory containing the MetaPhlan3 Database.
-                                 (The database will be automatically downloaded if it this directory is not set.)
-    -t    NUM                    Number of threads to use. (default=1)
-    -opts metaphlan3_options     metaphlan3's options.
-    -h,   --help                 Show this help.
+Options:
+-d    db_dir      Directory containing the MetaPhlAn3 Database.
+                  The database will be automatically downloaded if it this directory is not set.
+-t    NUM         Number of threads to use. (default=1)
+-opts OPTIONS     MetaPhlAn3's options.
+-h, --help      Show this help.
 
 HELP_USAGE
 }
 
-##--------------------------Exiting if input files are missing---------------##:
+# Exit if command is called with no arguments
 validate_arguments "$#"
-##----------------------Saving input orders into variables------------------##:
+
+# Parse arguments
 while (("$#")); do
     case "$1" in
         -h|--help ) usage; exit 0;;
@@ -42,20 +42,25 @@ while (("$#")); do
         * )         echo "Option '$1' not recognized"; exit 1;;
     esac
 done
-##----------------Verify that input directory exists------------------------##:
+
+# Verify that input directory exists
 validate_input_dir
-##---------------Create output directory if it doesn't exists---------------##:
+
+# Create output directory if it doesn't exists
 validate_output_dir
-##----------------Activate conda environment--------------------------------##:
+
+# Activate conda environment
 activate_env metabiome-taxonomic-profiling
-##----------------Installing MetaPhlAn database-----------------------------##:
-##First checks if MetaPhlAn3 database already generated, otherwise it will be generated.
+
+# Install MetaPhlAn database
+# First check if MetaPhlAn3 database already generated, otherwise it will be generated.
 if [[ ! -d "$met_database" ]];then
     create_dir "$out_dir" database
     met_database="$out_dir"/database
     metaphlan --install --bowtie2db "$met_database"
 fi
-##---------------------Output info----------------------------------------##:
+
+# Output info
 echo "Conda environment: $CONDA_DEFAULT_ENV"
 echo "Input directory: $input_dir"
 echo "Output directory: $out_dir"
@@ -63,7 +68,8 @@ echo "Number of threads: ${threads:=1}"
 echo "MetaPhlAn3 database: ${met_database:?'Database not downloaded'}"
 echo "MetaPhlAn3 version: $(metaphlan -v)"
 echo "MetaPhlAn3 called with options: $metaphlan_opts"
-##---------------------------MetaPhlAn profiling----------------------------##:
+
+# MetaPhlAn profiling
 for file in "$input_dir"/*; do
     # Paired end reads
     if [[ "$file" == @(*_R1_*|*_1).@(fq|fastq|fq.gz|fastq.gz) ]]; then
@@ -74,6 +80,7 @@ for file in "$input_dir"/*; do
             -o "$out_dir"/$(echo "$core_name" | sed 's/_bt2/_mphlan/').txt \
             --nproc "$threads" --bowtie2out "$out_dir"/$(echo "$core_name" | sed 's/_bt2/_mphlan/').sam \
             $metaphlan_opts
+
     # Single end reads
     elif [[ ! "$file" ==  *_@(*R1_*|*1.|*R2_*|*2.)* ]] && [[ "$file" == *.@(fq|fastq|fq.gz|fastq.gz) ]]; then
         unpaired_file="$file"
@@ -82,15 +89,17 @@ for file in "$input_dir"/*; do
             -o "$out_dir"/$(echo "$core_name" | sed 's/_bt2/_mphlan/').txt \
             --nproc "$threads" --bowtie2out "$out_dir"/$(echo "$core_name" | sed 's/_bt2/unpaired_mphlan/').sam \
             $metaphlan_opts
-    #Files that do not match the required extension:
+
+    # Files that do not match the required extension:
     elif [[ ! "$file" == *.@(fq|fastq|fq.gz|fastq.gz) ]]; then
         echo -e "$(basename -- "$file") will not be processed as is not a .fastq or .fq.gz file."
     fi
 done
-##-----------------------------Merging tables from utility scripts-----------##
+
+# Merge tables from utility scripts
 cd "$out_dir"
 merge_metaphlan_tables.py *.txt > merged_mphlan.txt
 
 echo "Done."
-echo "You can now use this metagenomic profiling to :"
+echo "You can now use this metagenomic profiling to:"
 echo "- Play around in R or Python"
