db2054a20a579375c862e89862fbdd4991b82f3d
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2021-04-12 18:15:07 -0500

    Write header for config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# Configuration info created at install
+# install.sh writes into this file, uninstall.sh reads from it
 
 COMPLETION_DIR=${BASH_COMPLETION_USER_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion}/completions
